9ad9d447321f4f02203d5c4459bd3e01bc2039e7
Raymond Lee <raylee@ucsc.edu>
2020-11-07 15:46:43 -0800

    d

---
+++ bulma/index.js
@@ -0,0 +1,10 @@
+//mobile menu
+
+const burgerIcon = document.getElementById('burger');
+const navbarMenu = document.getElementById('nav-links');
+const tlLogo = document.getElementById('tl-logo');
+
+burgerIcon.addEventListener('click', function(){navbarMenu.classList.toggle('is-active')});
+tlLogo.addEventListener('click', function(){
+    
+})
