37fdb2814cd8773fbcb6f68d1b8e00b2ee0c6873
Raymond Lee <raylee@ucsc.edu>
2020-11-08 00:50:11 -0800

    finished body

--- bulma/index.js
+++ bulma/index.js
@@ -5,6 +5,46 @@ const navbarMenu = document.getElementById('nav-links');
 const tlLogo = document.getElementById('tl-logo');
 
 burgerIcon.addEventListener('click', function(){navbarMenu.classList.toggle('is-active')});
-tlLogo.addEventListener('click', function(){
-    
+
+const modalBG = document.querySelector('.modal-background');
+
+
+const havenModal = document.getElementById('havenRate');
+const havenclose = document.getElementById('havenClose');
+const havenIcon = document.getElementById('havenstats');
+havenIcon.addEventListener('click', function(){
+    havenModal.classList.add('is-active');
+})
+havenclose.addEventListener('click', function(){
+    havenModal.classList.remove('is-active');
+})
+
+const bindModal = document.getElementById('bindRate');
+const bindclose = document.getElementById('bindClose');
+const bindIcon = document.getElementById('bindstats');
+bindIcon.addEventListener('click', function(){
+    bindModal.classList.add('is-active');
+})
+bindclose.addEventListener('click', function(){
+    bindModal.classList.remove('is-active');
+})
+
+const splitModal = document.getElementById('splitRate');
+const splitclose = document.getElementById('splitClose');
+const splitIcon = document.getElementById('splitstats');
+splitIcon.addEventListener('click', function(){
+    splitModal.classList.add('is-active');
+})
+splitclose.addEventListener('click', function(){
+    splitModal.classList.remove('is-active');
+})
+
+const ascentModal = document.getElementById('ascentRate');
+const ascentclose = document.getElementById('ascentClose');
+const ascentIcon = document.getElementById('ascentstats');
+ascentIcon.addEventListener('click', function(){
+    ascentModal.classList.add('is-active');
+})
+ascentclose.addEventListener('click', function(){
+    ascentModal.classList.remove('is-active');
 })
